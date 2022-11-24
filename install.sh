38cd31b97a4f9b8cb79a6e21e77fbcbc65450a55
khaled yassen <79770764+khaledyassen@users.noreply.github.com>
2022-11-25 00:02:21 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install github.com/tomnomnom/unfurl@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go get -u github.com/tomnomnom/assetfinder
+go install github.com/projectdiscovery/katana/cmd/katana@latest
+go install -v github.com/lukasikic/subzy@latest
+go install -v github.com/tomnomnom/anew@latest
+pip3 install uro
