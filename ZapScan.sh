e7d935b97a768216227b301b90fa4be8c291d154
khaled yassen <79770764+khaledyassen@users.noreply.github.com>
2024-02-08 02:22:48 +0200

    Update ZapScan.sh

--- ZapScan.sh
+++ ZapScan.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Prompt the user to enter a file path or name
+
 echo "Please enter the path or name of the file:"
 read file_input
 
-# Check if the entered file exists
+# Check if file exists
 if [ -e "$file_input" ]; then
     echo "File '$file_input' exists."
-    # You can perform further actions on the file here
 else
     echo "File '$file_input' does not exist or is inaccessible."
 fi
@@ -18,10 +17,8 @@ counter=1
 # Create the output directory if it doesn't exist
 mkdir -p "$OUTPUT_DIRECTORY"
 
-# Read the URLs from the input file and iterate over them
+# Read the URLs from the input file
 while read -r url; do
-    # Generate the output filename based on the counter    
-    
     # Run the tool for the URL and save the output to the output file
     zaproxy -cmd -quickurl "$url" -quickprogress -quickout "$pwd"output/output_$counter.xml
     
